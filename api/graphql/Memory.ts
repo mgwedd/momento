ba1985997cc6a27f938b08579ac86d55c6853493
Michael <michael.wedd@gmail.com>
2022-06-04 12:14:51 -0400

    wip mem pagination via relay spec

--- api/graphql/Memory.ts
+++ api/graphql/Memory.ts
@@ -52,7 +52,6 @@ export const Response = objectType({
 // client sends a request. 
 // This be either be the first req, send another req, which will contain a cursor
 
-
 export const MemoryQuery = extendType({
   type: 'Query',                        
   definition(t) {
@@ -63,27 +62,86 @@ export const MemoryQuery = extendType({
         after: stringArg()
       },
       async resolve(_, args, ctx) {
-        return {
-          edges: [
-            { 
-              cursor: '', 
-              node: {
-                id: '1asd',
-                title: 'Origin Memory',
-                body: '...', 
-                isPublished: false,
-                userId: 'abc123',
-                story: 'my story',
-                memoryOriginatedAt: new Date(), 
-                createdAt: new Date()
-              }
+        let queryResults = null
+        if (args.after) {
+          // check if there is a cursor as the argument
+          queryResults = ctx.prisma.link.findMany({
+            take: args.first, // the number of items to return from the db
+            skip: 1, // skip the cursor itself
+            cursor: {
+              id: args.after // the cursor
             }
-          ],
-          pageInfo: { 
-            endCursor: '',
-            hasNextPage: false
+          })
+        } else {
+          // if no cursor, this means that this is the first request
+          // we wil return the first items in the db
+          queryResults = await ctx.prisma.link.findMany({
+            take: args.first,
+          })
+        }
+
+        if (queryResults.length < 1) {
+          // no results found
+          return {
+            pageInfo: { 
+              endCursor: null, 
+              hasNextPage: false, 
+              edges: []
+            }
           }
         }
+
+        // the query has returned memories, so figure out the page info (more pages?) and return results
+
+        // get the last element in the previous result set
+        const lastMemoriesInResults = queryResults[queryResults.length - 1]
+        // cursor we'll return in subsequent requests
+        const { id : newCursor } = lastMemoriesInResults
+        // query after the cursor to check if we have a next page of results
+        const secondQueryResults = await ctx.prisma.link.findMany({ 
+          take: args.first, 
+          cursor: { 
+            id: newCursor
+          }
+        })
+
+        // return the response of the initial query
+        const result =  {
+          pageInfo: { 
+            endCursor: newCursor, 
+            // we have a next page if the number of items is greater than the response of the second query
+            hasNextPage: secondQueryResults.length > args.first
+          },
+          edges: queryResults.map( memory => ({
+            cursor: memory.id, 
+            node: memory
+          }))
+        }
+
+        return result
+
+
+        // return {
+        //   edges: [
+        //     { 
+        //       cursor: '', 
+        //       node: {
+        //         id: '1asd',
+        //         title: 'Origin Memory',
+        //         body: '...', 
+        //         isPublished: false,
+        //         userId: 'abc123',
+        //         story: 'my story',
+        //         memoryOriginatedAt: new Date(), 
+        //         createdAt: new Date()
+        //       }
+        //     }
+        //   ],
+        //   pageInfo: { 
+        //     endCursor: '',
+        //     hasNextPage: false
+        //   }
+        // }
       }
     })
   }
