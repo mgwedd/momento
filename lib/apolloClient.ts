e440395bb73a7ffb08d0a2510dc52e672172d1f4
Michael <michael.wedd@gmail.com>
2022-06-23 23:16:55 -0400

    FIX apollo client relay fields name

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -7,7 +7,7 @@ export const client = new ApolloClient({
     typePolicies: {
       Query: {
         fields: {
-          links: relayStylePagination()
+          memories: relayStylePagination()
         }
       }
     }
