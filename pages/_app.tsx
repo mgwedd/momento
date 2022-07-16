a8dead07ded5c84baed32accefdd1e8a3a976817
Michael <michael.wedd@gmail.com>
2022-07-16 17:46:42 -0400

    FIX location of theme within pages is incorrect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { UserProvider } from '@auth0/nextjs-auth0';
 import { ApolloProvider } from "@apollo/client";
 
-import { theme } from './theme'
+import { theme } from '../utils'
 import { client } from "../lib/apolloClient"
 
 export default function App({ Component, pageProps }) {
